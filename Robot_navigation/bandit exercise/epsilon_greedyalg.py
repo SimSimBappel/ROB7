efbac41448a552042c9f75dbd5c4cc71ece44d58
simsimbappel <simontk2010@hotmail.com>
2023-11-07 11:24:18 +0100

    comments

--- Robot_navigation/bandit exercise/epsilon_greedyalg.py
+++ Robot_navigation/bandit exercise/epsilon_greedyalg.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 # Define Action class 
-class Actions: 
+class Bandit: 
     def __init__(self, m): 
         self.m = m 
         self.mean = 0
         self.N = 0
 
-    # Choose a random action 
+    # Random action 
     def choose(self): 
         return np.random.randn() + self.m 
 
@@ -18,16 +18,14 @@ class Actions:
         self.mean = (1 - 1.0 / self.N)*self.mean + 1.0 / self.N * x 
 
 
-def run_experiment(m1, m2, m3, eps, N): 
+def greedy(m1, m2, m3, eps, N): 
         
-    actions = [Actions(m1), Actions(m2), Actions(m3)] 
+    actions = [Bandit(m1), Bandit(m2), Bandit(m3)] 
 
     data = np.empty(N) 
         
     for i in range(N): 
-        # epsilon greedy 
-        p = np.random.random() 
-        if p < eps: 
+        if np.random.random() < eps: 
             j = np.random.choice(3) 
         else: 
             j = np.argmax([a.mean for a in actions]) 
@@ -36,9 +34,9 @@ def run_experiment(m1, m2, m3, eps, N):
 
         # for the plot 
         data[i] = x 
-    cumulative_average = np.cumsum(data) / (np.arange(N) + 1) 
+    cumulative_average = np.cumsum(data) / (np.arange(N) +1) 
 
-
+    # Sanity check
     for a in actions: 
         print(a.mean) 
 
@@ -46,15 +44,15 @@ def run_experiment(m1, m2, m3, eps, N):
 
 
 
-        
-c_1 = run_experiment(1.0, 2.0, 3.0, 0.1, 100000) 
-c_05 = run_experiment(1.0, 2.0, 3.0, 0.05, 100000) 
-c_01 = run_experiment(1.0, 2.0, 3.0, 0.01, 100000) 
-# log scale plot 
-plt.figure(figsize = (12, 8))
-plt.plot(c_1, label ='eps = 0.1') 
-plt.plot(c_05, label ='eps = 0.05') 
-plt.plot(c_01, label ='eps = 0.01') 
+# Formatting: mean of actions 1,2,3, epsilon, N 
+eps_1 = greedy(1.0, 2.0, 3.0, 0.1, 100000) 
+eps_05 = greedy(1.0, 2.0, 3.0, 0.05, 100000) 
+eps_01 = greedy(1.0, 2.0, 3.0, 0.01, 100000) 
+# Plot all experiments
+plt.figure(figsize = (10, 6))
+plt.plot(eps_01, label ='epilons = 0.01') 
+plt.plot(eps_05, label ='epsilon = 0.05') 
+plt.plot(eps_1, label ='epsilon = 0.1') 
 plt.legend() 
 plt.xscale('log') 
 plt.show() 
